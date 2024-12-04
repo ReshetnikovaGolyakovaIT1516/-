3449f14a48362ca4ea3790d19d121cc69d563d89
ReshetnikovaGolyakovaIT1516 <golakovak3@gmail.com>
2024-12-04 13:58:14 +0500

    Функция для отображения состояния игры

--- игра виселица.cpp
+++ игра виселица.cpp
@@ -1,2 +1,28 @@
 ﻿#include <iostream>
 #include <string>
+#include <iomanip>
+// Функция для отображения состояния игры
+void displayState(const string& word, const string& guessed)
+{
+    for (char c : word)
+    {
+        bool found = false;
+        for (char g : guessed)
+        {
+            if (c == g)
+            {
+                found = true; // Буква найдена среди угаданных
+                break;
+            }
+        }
+        if (found)
+        {
+            cout << c << " "; // Если буква угадана, показываем её
+        }
+        else
+        {
+            cout << "_ ";
+        }
+    }
+    cout << endl;
+}
