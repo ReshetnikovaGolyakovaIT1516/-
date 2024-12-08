d9f2a3f42464d3ab62e9cdf6e4dec2f59f1e4594
Анастасия Решетникова <nastya-rechreshetnikova@yandex.ru>
2024-12-08 11:33:57 +0500

    Основная функция для проверки есть ли буква в слове

--- игра виселица.cpp
+++ игра виселица.cpp
@@ -59,7 +59,56 @@ int main()
     string words[] = { "программист", "алгоритм", "компьютер", "виселица", "разработка", "яблоко", "груша", "стол", "программа", "интернет", "телефон", "процессор", "ламинария" };
     srand(time(0));
     string word = words[rand() % (sizeof(words) / sizeof(words[0]))];
+    int maxAttempts = 6; // макс кол-во попыток
+    int attempts = 0; // кол-во неверных попыток
+    string guessed; // для хранения угаданных букв
     cout << "Добро пожаловать в игру Виселица!" << endl;
+    while (attempts < maxAttempts)
+    {
+        displayState(word, guessed); // текущее состояние слова
+        cout << "Введите букву: ";
+        char guess;
+        cin >> guess;
+        guess = tolower(guess);
+        if (!checkGuess(guess, word, guessed))// была ли буква угадана
+        {
+            attempts++;
+            cout << "Неправильно! Осталось попыток: " << (maxAttempts - attempts) << endl;
+        }
+        else
+        {
+            cout << "Верно!" << endl;
+        }
+        bool wordGuessed = true;// угадано ли все слово
+        for (char c : word)
+        {
+            bool found = false;
+            for (char g : guessed)
+            {
+                if (c == g)
+                {
+                    found = true; // найдена в угаданных
+                    break;
+                }
+            }
+            if (!found)
+            {
+                wordGuessed = false; // если хотя бы одна буква не найдена
+                break;
+            }
+        }
+
+        if (wordGuessed)
+        {
+            cout << "Поздравляем! Вы угадали слово: " << word << endl;
+            break;
+        }
+    }
+
+    if (attempts == maxAttempts)
+    {
+        cout << "Вы проиграли! Загаданное слово было: " << word << endl;
+    }
 
     return 0;
 }
